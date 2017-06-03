4034eb25edde61f33ed4cee04d469b07cc3db11e
nekomeo <linh.tu545@gmail.com>
2017-06-02 17:40:00 -0700

    Initial Commit

---
+++ PigLatin/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PigLatin
+//
+//  Created by Elle Ti on 2017-06-02.
+//  Copyright © 2017 Elle Ti. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
